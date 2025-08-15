9538e918e8000b48b68b3312661118d4d4421067
KOREADEVELOPERS <sulabhsharma3624@gmail.com>
2025-08-15 21:52:55 +0530

    Update repository.java

--- src/main/java/student_mang_sys/Std_system/repository.java
+++ src/main/java/student_mang_sys/Std_system/repository.java
@@ -1,15 +1,10 @@
 package student_mang_sys.Std_system;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
+import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface repository extends JpaRepository<std_Attribute, Long> {
-    @Query("SELECT s FROM std_Attribute s WHERE LOWER(s.name) LIKE LOWER(CONCAT('%', :query, '%')) OR LOWER(s.email) LIKE LOWER(CONCAT('%', :query, '%')) OR s.phone LIKE CONCAT('%', :query, '%')")
-    List<std_Attribute> searchStudents(@Param("query") String query);
+public interface repository extends MongoRepository<std_Attribute, String> {
     Optional<std_Attribute> findByEmailAndPassword(String email, String password);
     List<std_Attribute> findByCreatedBy(String email);
 }
