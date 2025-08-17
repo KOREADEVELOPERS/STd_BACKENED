014c585611b2e4230a8b282dde7faeb70a8fb58a
KOREADEVELOPERS <sulabhsharma3624@gmail.com>
2025-08-17 12:01:33 +0530

    Update std_Attribute.java

--- src/main/java/student_mang_sys/Std_system/std_Attribute.java
+++ src/main/java/student_mang_sys/Std_system/std_Attribute.java
@@ -1,23 +1,75 @@
 package student_mang_sys.Std_system;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Document(collection = "studentmanagement") // MongoDB collection ka naam
+@Document(collection = "studentmanagement")
 public class std_Attribute {
 
     @Id
-    private String id; // MongoDB String type ka ObjectId hota hai
-
+    private String id;
     private String name;
     private String email;
     private String phone;
     private String password;
     private String createdBy;
+
+    public std_Attribute() {}
+
+    public std_Attribute(String id, String name, String email, String phone, String password, String createdBy) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+        this.createdBy = createdBy;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
 }
