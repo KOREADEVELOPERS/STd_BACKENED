a827a6a4eb0c1d03641dafe6011a86a730235493
KOREADEVELOPERS <sulabhsharma3624@gmail.com>
2025-12-12 20:03:02 +0530

    Update std_service.java

--- src/main/java/student_mang_sys/Std_system/std_service.java
+++ src/main/java/student_mang_sys/Std_system/std_service.java
@@ -5,11 +5,7 @@ import java.util.List;
 public interface std_service {
     String Addingstudents(std_Attribute std);
     List<std_Attribute> showemoloyee();
-    String deletestudent(String id);
+    String deletestudent(String id);  // ✅ Changed to String
     String saveAllStudents(List<std_Attribute> students);
     List<std_Attribute> getstudent();
-
-    // 🔹 New methods for fetch/update by ID
-    std_Attribute getStudentById(String id);
-    std_Attribute updateStudent(String id, std_Attribute updatedStudent);
 }
